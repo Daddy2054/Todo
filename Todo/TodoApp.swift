5147c4ef1c25236ae4a8d0588148e374db1bd7dd
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-09 18:33:46 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Jean on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
